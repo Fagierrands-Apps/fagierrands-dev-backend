6c61959bbb1295eaeb7d8240773c5f1d04312612
dallah-254 <dallaherick0@gmail.com>
2026-05-25 09:24:17 +0300

    Fix database configuration to use DATABASE_URL

--- config/settings.py
+++ config/settings.py
@@ -72,14 +72,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASS'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 # Password validation
