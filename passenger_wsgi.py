b2faf23ec24796ea0deebb7ed3eb28574e016b34
dallah-254 <dallaherick0@gmail.com>
2026-05-31 22:45:36 +0300

    Final fix: use fagierrandsbackup to match cPanel folder

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,6 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'fagierrandsbackend'))
+sys.path.insert(0, os.path.dirname(__file__))
 
-from fagierrandsbackend.wsgi import application
+from fagierrandsbackup.wsgi import application
