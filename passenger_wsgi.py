d6c7d3fb4075b17c6b94dde3530b1858aefe6481
dallah-254 <dallaherick0@gmail.com>
2026-05-31 22:25:11 +0300

    Fix module name in passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,6 @@
 import os
 import sys
 
-# Add the application directory to the path
 sys.path.insert(0, os.path.dirname(__file__))
 
-# Import the Django WSGI application
-from fagierrandsbackend.wsgi import application
+from fagierrandsbackup.wsgi import application
