9521dd18c2bb62fce994662d3dd5c3e63ba9e2a1
dallah-254 <dallaherick0@gmail.com>
2026-06-03 13:41:58 +0300

    Fix cPanel deployment: Add OpenBLAS limit, simplify requirements, update Python version

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,11 @@
 import os
 import sys
 
+# Fix OpenBLAS thread limit to prevent resource exhaustion
+os.environ['OPENBLAS_NUM_THREADS'] = '4'
+
 sys.path.insert(0, os.path.dirname(__file__))
 
-# Use same settings as Render - will auto-detect cPanel vs Render
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fagierrandsbackup.settings')
 
 from django.core.wsgi import get_wsgi_application
