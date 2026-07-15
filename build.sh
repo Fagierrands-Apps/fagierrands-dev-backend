87f30ee244ae6d11bfb8e9ce63b491413fac0259
bluetech254 <bluetech1bt@gmail.com>
2026-07-15 14:10:13 +0300

    add admin user credentials to system for the redner dev databse

--- build.sh
+++ build.sh
@@ -4,4 +4,18 @@ set -e
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-python manage.py create_admin
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+import os
+username = os.environ.get('DJANGO_ADMIN_USER')
+email = os.environ.get('DJANGO_ADMIN_EMAIL')
+password = os.environ.get('DJANGO_ADMIN_PASSWORD')
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print('Admin user created.')
+else:
+    print('Admin user already exists or env vars not set.')
+"
+
