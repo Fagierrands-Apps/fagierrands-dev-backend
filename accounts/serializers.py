8ab0e3753d0dc1ae1da20fe6c4cb51f5177fd028
fagierrands1 <fagierrands1@gmail.com>
2026-07-18 10:17:58 +0300

    fix: remove uppercase letter requirement from password validation

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -51,9 +51,6 @@ class RegisterSerializer(serializers.ModelSerializer):
         if len(value) < 8:
             raise serializers.ValidationError("Password must be at least 8 characters long")
         
-        if not re.search(r'[A-Z]', value):
-            raise serializers.ValidationError("Password must contain at least one uppercase letter")
-        
         if not re.search(r'[a-z]', value):
             raise serializers.ValidationError("Password must contain at least one lowercase letter")
         
